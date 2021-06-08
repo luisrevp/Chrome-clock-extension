4e61df9e76e884d77866d1a4c2cb10ed41bb060b
Luis Revilla <luisrev97@gmail.com>
2021-06-08 16:33:10 -0300

    cargado proyecto

---
+++ README.md
@@ -0,0 +1,12 @@
+### COMO USAR EXTENSIÓN
+
+# válido solo para Google Chrome
+
+1. Descargar todos los ficheros incluidos en el repositorio y ubicar en una carpeta específica
+2. Abrir Chrome --> Más erramientas --> Extensiones
+3. En el apartado de extensiones, presionar "carga descomprimida".
+4. Nos va a pedir seleccionar una carpeta, que en este caso es la que creamos
+5. Una vez cargada, nos lo indicará en el listado de extensiones
+6. Probar la extensión abriendo una pestaña nueva
+   - Podemos alternar entre modo diurno/nocturno
+   - En caso de querer desactivar, en el mismo panel de extensiones la podremos desactivar/eliminar
---
+++ reloj.js
@@ -0,0 +1,75 @@
+/*
+combinar: 
+
+1- usar un setTimeout (esta función tomará como callback los elementos que componen al reloj y a la función misma
+2- Recursividad (la función se ejecutará a sí misma hasta cumplir con la condición (cada segundo))
+3- usar el Date constructor para crear los elementos de fecha y hora que cambiarán en tiempo real
+
+*/
+
+console.log(
+  "%cHola! Soy Luis Revilla",
+  "color: red; font-size: 35px; text-align: center"
+);
+console.log(
+  "%cBienvenidos a mi extensión de Chrome. Siéntete cómodo inspeccionando los elementos que contiene",
+  "color: green; font-size: 25px; text-align: center"
+);
+
+/*FUNCIONAMIENTO DEL RELOJ*/
+function reloj() {
+  let mainPage = document.getElementById("reloj-main");
+  let newDate, newHours, horas, minutos, segundos, dias, meses;
+  let date = new Date();
+  horas = date.getHours();
+  minutos = date.getMinutes();
+  segundos = date.getSeconds();
+  dias = date.getDate();
+  meses = date.getMonth() + 1;
+
+  let datos = [horas, minutos, segundos, dias, meses].map((el) => {
+    return (el < 10 ? "0" : "") + el;
+  });
+
+  return setTimeout(() => {
+    newDate = `${datos[3]}/${datos[4]}/${date.getFullYear()}`;
+    newHours = `${datos[0]}:${datos[1]}:${datos[2]}`;
+    mainPage.innerHTML = `
+    <div class="date-wrap">
+    <h1 class="date">${newHours}</h1>
+    <h1 class="date">${newDate}</h1>
+    </div>
+    `;
+    reloj();
+  }, 1000);
+}
+
+reloj();
+
+/*CAMBIO MODO DIURNO/NOCTURNO*/
+function cambiarTema() {
+  let botonNight = document.getElementById("turn-night");
+  let day = document.querySelector(".day");
+  let night = document.querySelector(".night");
+
+  const changeDisplay = () => {
+    if (night.style.display != "block") {
+      botonNight.style = "animation: buttonMove1 600ms ease-in-out";
+      night.style = "display: block; opacity: 1";
+      day.style = "display: none; opacity: 0";
+      document.body.style = "background: #000; color: #fff";
+      document.querySelector(".date-wrap h1:first-child").style.color = "#fff";
+      document.querySelector("#turn-night img").src = "media/Day Button.svg";
+    } else {
+      botonNight.style = "animation: buttonMove2 600ms ease-in-out";
+      day.style = "display: block; opacity: 1";
+      night.style = "display: none; opacity: 0";
+      document.body.style = "background: #fff";
+      document.querySelector("#turn-night img").src = "media/Night Button.svg";
+    }
+  };
+
+  botonNight.addEventListener("click", changeDisplay);
+}
+
+cambiarTema();
